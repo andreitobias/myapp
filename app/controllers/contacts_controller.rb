6b42ca702eb5a7f0dc4b6b2ae9be25c629a0156c
Andrei Tobias <tobias_andrei@yahoo.com>
2017-07-02 15:21:36 +0000

    Contacts saved to DB

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,17 @@ class ContactsController < ApplicationController
   end
   
   def create
+    @contact = Contact.new(contact_params)
+    
+    if @contact.save
+      redirect_to root_path, notice: "Message sent."
+    else
+      redirect_to root_path, notice: "Error occured."
+    end  
   end  
+  
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :url) 
+    end  
 end
