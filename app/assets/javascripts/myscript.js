cc884e95d655d3d87903fa7c314a523d1dbbb87e
Andrei Tobias <tobias_andrei@yahoo.com>
2017-07-20 19:05:00 +0000

    Added styling and background

--- app/assets/javascripts/myscript.js
+++ app/assets/javascripts/myscript.js
@@ -7,4 +7,19 @@ $(function () {
       $(this).text(wordsArray[count % wordsArray.length]).fadeIn(400);
     });
   }, 4000);
+  
+  $('[data-popup-open]').on('click', function(e)  {
+        var targeted_popup_class = $(this).attr('data-popup-open');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+ 
+        e.preventDefault();
+    });
+ 
+    //----- CLOSE
+    $('[data-popup-close]').on('click', function(e)  {
+        var targeted_popup_class = $(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+ 
+        e.preventDefault();
+    });
 });
