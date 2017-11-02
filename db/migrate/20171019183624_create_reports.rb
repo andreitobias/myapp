e04e836744a1bdc6305620b0bfef15e765c9f8f0
Andrei Tobias <tobias_andrei@yahoo.com>
2017-11-02 18:46:22 +0000

    added refferencese to migrations

--- db/migrate/20171019183624_create_reports.rb
+++ db/migrate/20171019183624_create_reports.rb
@@ -2,7 +2,7 @@ class CreateReports < ActiveRecord::Migration[5.1]
   drop_table :reports
   def change
     create_table :reports do |t|
-      t.integer :contact_id
+      t.integer :contact_id, references: nil
       t.string :impression
       t.string :mobile
       t.integer :speed
